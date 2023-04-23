e8b42dcea902730c27110cae10e055af29f50786
Ram Basnet <ram.einstein42@gmail.com>
2023-04-22 21:42:47 -0500

    invasion

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # initialize game
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # make a ship
+    ship = Ship(ai_settings, screen)
+
+    # set background color
+    bg_color = (230, 230, 230)
+
+    # start main loop for game
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
